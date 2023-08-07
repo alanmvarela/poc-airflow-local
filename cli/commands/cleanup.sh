97e661f71eff43de022d0732eb5ec877d5be5c89
Alan Varela <alan.martin.varela@gmail.com>
2023-08-07 12:25:29 -0600

    Update cleanup script

--- cli/commands/cleanup.sh
+++ cli/commands/cleanup.sh
@@ -7,6 +7,10 @@ cli_log "Removing all airflow local images and volumes"
 
 cd "$CLI_WORKDIR/airflow" || exit 1
 
+docker stop poc-airflow-local-postgres-1 poc-airflow-local-redis-1
+
+docker rm poc-airflow-local-postgres-1 poc-airflow-local-redis-1 
+
 docker compose down --volumes --rmi all
 
 cli_log "All airflow local images and volumes removed"
